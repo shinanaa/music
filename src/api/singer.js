dfd2bf85d08ae27ff8c3b9d77fac9b4eac286a16
shinanaa <916457399@qq.com>
2019-08-09 17:28:51 +0800

    1.singer.js文件编写，获取singer页面数据 2.listView基础组件的创建 3.通用js中添加singer.js，创建Singer类，设置其id，name，和头像 4.歌手页面编写获取页面数据以及过滤数据的方法 5.singer页面css的添加

--- src/api/singer.js
+++ src/api/singer.js
@@ -1,6 +1,17 @@
-// import Jsonp from 'common/js/jsonp'
-// import {commonParams, options} from './config'
-// // import axios from 'axios'
-// export function getSingerList() {
-//   const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg'
-// }
+import Jsonp from 'common/js/jsonp'
+import {commonParams, options} from './config'
+export function getSingerList() {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg'
+  const data = Object.assign({}, commonParams, {
+    channel: 'singer',
+    page: 'list',
+    key: 'all_all_all',
+    pagesize: 100,
+    pagenum: 1,
+    hostUin: 0,
+    needNewCode: 0,
+    platform: 'yqq.json',
+    g_tk: 5381
+  })
+  return Jsonp(url, data, options)
+}
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,15 +1,89 @@
 <template>
-    <div>
+    <div class="singer">
       歌手页面
     </div>
 </template>
 
 <script>
+import {getSingerList} from '../../api/singer'
+import {ERR_OK} from '../../api/config'
+import Singer from 'common/js/singer'
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
 export default {
-  name: 'singer'
+  name: 'singer',
+  data() {
+    return {
+      singers: []
+    }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.singers = res.data.list
+          console.log(this._normalizeSinger(this.singers))
+        }
+      })
+    },
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          item: []
+        }
+      }
+      list.forEach((item, index) => {
+        // 过滤数据取前10条作为热门歌手
+        if (index < HOT_SINGER_LEN) {
+          map.hot.item.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        }
+        // 根据Findex为每一条数据创建新的字母分组
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        }))
+      })
+      // 将map内容处理为所需的有序列表
+      let hot = []
+      let ret = []
+      // 将数据分为热门和其他两部分
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      // 将其他部分的数据按字母升序排序
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      // 返回合并后的一维数组
+      return hot.concat(ret)
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
