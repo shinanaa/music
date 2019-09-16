43cb07b065c53baacae48a800a8165064b7aed73
shinanaa <916457399@qq.com>
2019-09-16 17:19:14 +0800

    1.歌手页面添加fixed内容，实现歌手列表在滚动过程中分类title定位在顶部的效果 2.添加loading组件，在页面初始化数据时，显示loading效果过渡

--- src/base/listView/listView.vue
+++ src/base/listView/listView.vue
@@ -27,13 +27,21 @@
               :data-index="index">{{item}}</li>
         </ul>
       </div>
+      <div class="list-fixed" v-show="fixedTitle" ref="fixed">
+        <h1 class="fixed-title">{{fixedTitle}}</h1>
+      </div>
+      <div v-show="!data.length" class="loading-container">
+        <Loading></Loading>
+      </div>
     </scroll>
 </template>
 
 <script>
 import Scroll from '@/base/scroll/scroll'
+import Loading from '@/base/loading/loading'
 import {getData} from 'common/js/dom'
 const ANCHOR_HEIGHT = 18
+const TITLE_HEIGHT = 30
 export default {
   name: 'list-view',
   props: {
@@ -45,7 +53,8 @@ export default {
   data() {
     return {
       scrollY: -1,
-      currentIndex: 0
+      currentIndex: 0,
+      diff: -1
     }
   },
   computed: {
@@ -53,10 +62,17 @@ export default {
       return this.data.map((group) => {
         return group.title.substr(0, 1)
       })
+    },
+    fixedTitle() {
+      if (this.scrollY > 0) {
+        return ''
+      }
+      return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
     }
   },
   components: {
-    Scroll
+    Scroll,
+    Loading
   },
   created() {
     this.touch = {}
@@ -126,11 +142,20 @@ export default {
         let height2 = listHeight[i + 1]
         if (!height2 || (-newY >= height1 && -newY < height2)) {
           this.currentIndex = i
+          this.diff = height2 + newY
           return
         }
       }
       // 当滚动到底部 且-newY大于最后一个元素的上限
       this.currentIndex = listHeight.length - 2
+    },
+    diff(newVal) {
+      let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? newVal - TITLE_HEIGHT : 0
+      if (this.fixedTop === fixedTop) {
+        return
+      }
+      this.fixedTop = fixedTop
+      this.$refs.fixed.style.transform = `translate3d(0, ${fixedTop}px, 0)`
     }
   }
 }
