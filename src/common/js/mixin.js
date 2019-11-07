73268a5e8f7e49a720223a773eb6b55d7673087f
shinanaa <916457399@qq.com>
2019-11-07 09:17:45 +0800

    添加playermixin

--- src/common/js/mixin.js
+++ src/common/js/mixin.js
@@ -1,4 +1,6 @@
-import {mapGetters} from 'vuex'
+import {mapGetters, mapMutations} from 'vuex'
+import {playMode} from 'common/js/config'
+import {shuffle} from 'common/js/util'
 export const playListMixin = {
   computed: {
     ...mapGetters([
@@ -20,5 +22,48 @@ export const playListMixin = {
     handlePlayList() {
       throw new Error('component must implement handPlayList method')
     }
+  }
+}
+
+export const playerMixin = {
+  computed: {
+    iconMode() {
+      return this.mode === playMode.sequence ? 'icon-sequence' : this.mode === playMode.loop ? 'icon-loop' : 'icon-random'
+    },
+    ...mapGetters([
+      // 'fullScreen',
+      // 'playList',
+      'currentSong',
+      'playing',
+      'currentIndex',
+      'mode',
+      'sequenceList'
+    ])
+  },
+  methods: {
+    changeMode() {
+      const mode = (this.mode + 1) % 3
+      this.setPlayMode(mode)
+      let list = null
+      if (mode === playMode.random) {
+        list = shuffle(this.sequenceList)
+      } else {
+        list = this.sequenceList
+      }
+      this.resetCurrentIndex(list)
+      this.setPlayList(list)
+    },
+    resetCurrentIndex(list) {
+      let index = list.findIndex((item) => {
+        return item.id === this.currentSong.id
+      })
+      this.setCurrentIndex(index)
+    },
+    ...mapMutations({
+      setPlayingState: 'SET_PLAYING_STATE',
+      setCurrentIndex: 'SET_CURRENT_INDEX',
+      setPlayMode: 'SET_PLAY_MODE',
+      setPlayList: 'SET_PLAYLIST'
+    })
   }
 }
